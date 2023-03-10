2634a2872ce24acc6aafb7842cbbf7dd5173c76f
samuel <samuelbmx123@gmail.com>
2023-03-10 13:07:31 -0300

    configure openai

---
+++ src/config/openai.js
@@ -0,0 +1,23 @@
+import { Configuration, OpenAIApi } from "openai";
+
+class openai {
+  static configuration() {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    return new OpenAIApi(configuration);
+  }
+  static textCompletion({ prompt }) {
+    return {
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    };
+  }
+}
+
+export default new openai();
