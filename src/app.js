b37bf162b63a484a63ba8cfdd3c8dfd0690e829f
samuel <samuelbmx123@gmail.com>
2023-03-10 12:30:37 -0300

    import router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
+import router from "./routes/router.js";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 export default app;
